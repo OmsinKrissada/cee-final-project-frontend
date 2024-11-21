ac3deb7c19fc1e1bc194192ed54360fe27ba747e
Krissada Singhakachain <omsin_47@hotmail.com>
2024-11-21 12:30:14 +0700

    feat(game): allow pressing Enter to send text

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,36 +1,42 @@
 const config = {
     type: Phaser.AUTO,
-    width: window.innerWidth, 
-    height: window.innerHeight, 
+    width: window.innerWidth,
+    height: window.innerHeight,
     scene: {
         preload: preload,
         create: create,
         update: update,
     },
     scale: {
-        mode: Phaser.Scale.RESIZE, 
-        autoCenter: Phaser.Scale.CENTER_BOTH, 
+        mode: Phaser.Scale.RESIZE,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
     },
-    parent: 'phaser-container', 
-    transparent: true, 
+    parent: 'phaser-container',
+    transparent: true,
 };
 
 const game = new Phaser.Game(config);
 
 function preload() {
-    
+
+}
+
+const textField = document.getElementById("userInput");
+
+function sendText() {
+    textField.value = "";
 }
 
 function create() {
     const button = document.getElementById("clearButton");
-    button.addEventListener("click", () => {
-        const textField = document.getElementById("userInput");
-        textField.value = "";
+    textField.addEventListener('keypress', e => {
+        if (e.code == 'Enter') sendText();
     });
+    button.addEventListener("click", () => sendText());
 }
 
 function update() {
-    
+
 }
 
 window.addEventListener("resize", () => {
