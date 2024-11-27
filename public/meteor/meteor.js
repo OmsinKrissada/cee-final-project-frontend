77930cb31872667d665711a8742cfc52ddf2f3f7
Thanayot Chalernpornlert <6630120321@student.chula.ac.th>
2024-11-27 14:20:59 +0700

    first word fixed/meteor size dynamically change (a little buggy rn

--- public/meteor/meteor.js
+++ public/meteor/meteor.js
@@ -3,15 +3,15 @@ const sizes = {
   height: window.innerHeight,
 };
 
-const speedDown = 300; 
+const speedDown = 250;
 
 class GameScene extends Phaser.Scene {
   constructor() {
     super("scene-game");
-    this.smallMeteor = null;
-    this.largeMeteor = null;
-    this.smallWordBox = null;
-    this.largeWordBox = null;
+    this.firstMeteor = null;
+    this.secondMeteor = null;
+    // this.smallWordBox = null;
+    // this.largeWordBox = null;
     this.score = 0;
     this.scoreText = null;
     this.inputText = "";
@@ -22,22 +22,8 @@ class GameScene extends Phaser.Scene {
   }
 
   create() {
-    // Create meteors
-    // this.smallMeteor = this.physics.add.image(this.getRandomXForSmall(), 0, "met1")
-    //   .setOrigin(0, 0)
-    //   .setScale(0.1)
-    //   .setMaxVelocity(0, speedDown);
-
-    // this.largeMeteor = this.physics.add.image(this.getRandomXForLarge(), 0, "met1")
-    //   .setOrigin(0, 0)
-    //   .setScale(0.2)
-    //   .setMaxVelocity(0, speedDown - 100);
-
-    // this.smallWordBox = this.createFallingWordBox(this.getRandomXForSmall(), 0, 0.3);
-    // this.largeWordBox = this.createFallingWordBox(this.getRandomXForLarge(), 0, 0.5);
-
-    this.smallMeteor = this.createMeteorAndWord("small");
-    this.largeMeteor = this.createMeteorAndWord("large");
+    this.firstMeteor = this.createMeteorAndWord("game", this.getRandomXForSmall(), 0); // fix word "game" แก้ทีหลังได้
+    this.secondMeteor = this.createMeteorAndWord("start", this.getRandomXForLarge(), 0); // fix word "start" แก้ทีหลังได้
 
     this.scoreText = this.add.text(10, 10, 'Score: 0', {
       font: '20px Arial',
@@ -48,46 +34,27 @@ class GameScene extends Phaser.Scene {
 
     window.addEventListener("keydown", (event) => {
       if (event.key === "Enter") {
-        this.checkWordMatch(this.smallMeteor);
-        this.checkWordMatch(this.largeMeteor);
+        this.checkWordMatch(this.firstMeteor);
+        this.checkWordMatch(this.secondMeteor);
         this.inputField.value = "";  
       }
     });
   }
 
   update() {
-    // if (this.smallMeteor.y > sizes.height) {
-    //   this.smallMeteor.setY(0).setX(this.getRandomXForSmall()).setVelocityY(speedDown);
-    // }
-    // if (this.largeMeteor.y > sizes.height) {
-    //   this.largeMeteor.setY(0).setX(this.getRandomXForLarge()).setVelocityY(speedDown - 100);
-    // }
-
-
-    // this.smallWordBox.y += speedDown * 0.005;  
-    // this.largeWordBox.y += speedDown * 0.005;  
-
-
-    // if (this.smallWordBox.y > sizes.height) {
-    //   this.resetFallingWordBox(this.smallWordBox, this.getRandomXForSmall(), 0.3);
-    // }
-    // if (this.largeWordBox.y > sizes.height) {
-    //   this.resetFallingWordBox(this.largeWordBox, this.getRandomXForLarge(), 0.5);
-    // }
-    this.updateMeteorAndWord(this.smallMeteor, speedDown);
-    this.updateMeteorAndWord(this.largeMeteor, speedDown - 100);
+    this.updateMeteorAndWord(this.firstMeteor, speedDown);
+    this.updateMeteorAndWord(this.secondMeteor, speedDown - 100);
   }
 
-  createMeteorAndWord(size) {
-    const scale = size === "small" ? 0.1 : 0.2;
-    const xPosition = size === "small" ? this.getRandomXForSmall() : this.getRandomXForLarge();
+  createMeteorAndWord(word, xPosition, yPosition) {
+    const scale  = word.length <= 4 ? 0.1 : 0.2;
 
     const meteor = this.physics.add.image(xPosition, 0, "met1")
       .setOrigin(0, 0)
       .setScale(scale)
       .setMaxVelocity(0, speedDown);
 
-    const wordBox = this.add.text(xPosition + meteor.displayWidth / 2, meteor.displayHeight / 2, this.getRandomWord(), {
+    const wordBox = this.add.text(xPosition + meteor.displayWidth / 2, yPosition + meteor.displayHeight / 2, word, {
       backgroundColor: "#ffffff",
       font: `${Math.round(scale * 300)}px Arial`, 
       fill: "#000000",
@@ -96,7 +63,7 @@ class GameScene extends Phaser.Scene {
       wordWrap: { width: 200, useAdvancedWrap: true },
     }).setOrigin(0.5);
 
-    return { meteor, wordBox }; //for context kub : this used as pair in parameter for other function :)
+    return { meteor, wordBox };
   }
 
   updateMeteorAndWord(pair, velocityY) {
@@ -110,39 +77,15 @@ class GameScene extends Phaser.Scene {
   }
 
   resetMeteorAndWord(pair) {
-    const xPosition = pair.meteor.scaleX === 0.1 ? this.getRandomXForSmall() : this.getRandomXForLarge();
-    pair.meteor.setY(0).setX(xPosition);
-    pair.wordBox.setText(this.getRandomWord());
-  }
+    const newWord = this.getRandomWord(); 
+    const xPosition = newWord.length <= 4 ? this.getRandomXForSmall() : this.getRandomXForLarge();  
 
-  // createFallingWordBox(x, y, scale) {
-  //   const wordBox = this.add.text(x, y, this.getRandomWord(), {
-  //     font: `${Math.round(scale * 100)}px Arial`, 
-  //     fill: "#ffffff",
-  //     align: "center",
-  //     wordWrap: { width: 200, useAdvancedWrap: true },
-  //   }).setOrigin(0.5);
-
-  //   return wordBox;
-  // }
-
-  // resetFallingWordBox(wordBox, x, scale) {
-  //   wordBox.setY(0).setX(x);
-  //   wordBox.setText(this.getRandomWord()); 
-  // }
-
-  checkWordMatch(pair) {
-    this.inputText = this.inputField.value.trim().toLowerCase();
-
-    if (this.inputText === pair.wordBox.text.toLowerCase()) {
-      this.score += 10;
-      this.scoreText.setText('Score: ' + this.score);
-      this.resetMeteorAndWord(pair);
-    }
+    pair.meteor.setY(0).setX(xPosition).setScale(newWord.length <= 4 ? 0.1 : 0.2);
+    pair.wordBox.setText(newWord).setFont(`${Math.round(newWord.length <= 4 ? 0.1 * 300 : 0.2 * 300)}px Arial`);
   }
 
   getRandomWord() {
-    const words = ['sky', 'energy', 'meteor', 'stars', 'fall', 'explosion', 'space'];
+    const words = ['game', 'start', 'sky', 'energy', 'meteor', 'stars', 'fall', 'explosion', 'space'];
     return words[Phaser.Math.Between(0, words.length - 1)];
   }
 
@@ -153,7 +96,16 @@ class GameScene extends Phaser.Scene {
   getRandomXForLarge() {
     return Phaser.Math.Between(300, sizes.width - 300);
   }
+
+  checkWordMatch(pair) {
+    if (this.inputField.value.toLowerCase() === pair.wordBox.text.toLowerCase()) {
+      this.score += 10;
+      this.scoreText.setText('Score: ' + this.score);
+      this.resetMeteorAndWord(pair); 
+    }
+  }
 }
+
 
 const config = {
   type: Phaser.WEBGL,
