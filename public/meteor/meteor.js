98452f54432162ea306529feae90607b4b504189
Krissada Singhakachain <omsin_47@hotmail.com>
2024-12-01 19:32:17 +0700

    feat: basic telemetry

--- public/meteor/meteor.js
+++ public/meteor/meteor.js
@@ -1,39 +1,70 @@
+import { BACKEND_URL } from "../scripts/config.js";
+import api from "../scripts/api.js";
+
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
 
-const speedDown = 175; 
-const words = ['game', 'start', 'sky', 'energy', 'meteor', 'stars', 'fall', 'explosion', 'space', 'implement'];
+const speedDown = 175;
+// const words = ['game', 'start', 'sky', 'energy', 'meteor', 'stars', 'fall', 'explosion', 'space', 'implement'];
 
 class GameScene extends Phaser.Scene {
   constructor() {
     super("scene-game");
-    this.meteors = [];  
+    this.meteors = [];
     this.score = 0;
     this.inputText = "";
   }
 
+
+  // telemetry
+
+  async connectWithSSE() {
+    const res = await api.get(`/game/playtoken`);
+    if (!res || !res.token) window.location.href = '/';
+
+    const es = new EventSource(`${BACKEND_URL}/game/stream/play/${res.token}`);
+
+    es.addEventListener("word", (msg) => {
+      const { word } = JSON.parse(msg.data);
+      console.log(`got word: ${word}`);
+      this.addMeteor(word);
+    });
+
+    es.addEventListener("conclude", (msg) => {
+      const body = JSON.parse(msg.data);
+      console.log('got update');
+      renderRooms(body);
+    });
+
+    es.onopen = () => {
+      console.log("open");
+    };
+  }
+
+
   preload() {
-    this.load.image("met1", "../assets/AllMeteor/meteor7.png");  
+    this.load.image("met1", "../assets/AllMeteor/meteor7.png");
   }
 
   create() {
-    this.addMeteor();  
-    this.addMeteor();
+    this.connectWithSSE();
+    // this.addMeteor();
+    // this.addMeteor();
 
     this.inputField = document.getElementById("userInput");
 
     window.addEventListener("keydown", (event) => {
       if (event.key === "Enter") {
         this.meteors.forEach(meteor => this.checkWordMatch(meteor));
-        this.inputField.value = "";  
+        this.inputField.value = "";
       }
     });
 
-    setInterval(() => {
-      this.addMeteor(); 
-    }, 30000); 
+    // setInterval(() => {
+    //   this.addMeteor();
+    // }, 30000);
 
     this.updateScoreDisplay();
   }
@@ -44,8 +75,7 @@ class GameScene extends Phaser.Scene {
     });
   }
 
-  addMeteor() {
-    const word = this.getRandomWord();
+  addMeteor(word) {
     const xPosition = this.getRandomXForPosition();
     const yPosition = 0;
 
@@ -75,28 +105,33 @@ class GameScene extends Phaser.Scene {
     meteorData.wordBox.setPosition(meteorData.meteor.x + meteorData.meteor.displayWidth / 2, meteorData.meteor.y + meteorData.meteor.displayHeight / 2);
 
     if (meteorData.meteor.y > sizes.height) {
-      this.resetMeteor(meteorData);
+      this.deleteMeteor(meteorData);
     }
   }
 
-  resetMeteor(meteorData) {
-    meteorData.meteor.y = 0;
-    meteorData.meteor.x = this.getRandomXForPosition();
-    meteorData.wordBox.setPosition(meteorData.meteor.x + meteorData.meteor.displayWidth / 2, meteorData.meteor.y + meteorData.meteor.displayHeight / 2);
-    meteorData.word = this.getRandomWord();
+  // resetMeteor(meteorData) {
+  //   meteorData.meteor.y = 0;
+  //   meteorData.meteor.x = this.getRandomXForPosition();
+  //   meteorData.wordBox.setPosition(meteorData.meteor.x + meteorData.meteor.displayWidth / 2, meteorData.meteor.y + meteorData.meteor.displayHeight / 2);
+  // meteorData.word = this.getRandomWord();
 
-    let temp_scale = meteorData.word.length * 0.1;
-    
-    meteorData.meteor.setScale(temp_scale);
-    meteorData.wordBox.setText(meteorData.word); 
-    meteorData.wordBox.setScale(temp_scale);
+  //   let temp_scale = meteorData.word.length * 0.1;
+
+  //   meteorData.meteor.setScale(temp_scale);
+  //   meteorData.wordBox.setText(meteorData.word);
+  //   meteorData.wordBox.setScale(temp_scale);
+  // }
+
+  deleteMeteor(meteorData) {
+    meteorData.meteor.destroy();
+    meteorData.wordBox.destroy();
   }
 
   checkWordMatch(meteorData) {
     if (this.inputField.value === meteorData.word) {
       this.score++;
-      this.updateScoreDisplay(); 
-      this.resetMeteor(meteorData);
+      this.updateScoreDisplay();
+      this.deleteMeteor(meteorData);
     }
   }
 
@@ -105,12 +140,12 @@ class GameScene extends Phaser.Scene {
     scoreText.innerText = `MyScore : ${this.score}`;
   }
 
-  getRandomWord() {
-    return words[Math.floor(Math.random() * words.length)];
-  }
+  // getRandomWord() {
+  //   return words[Math.floor(Math.random() * words.length)];
+  // }
 
   getRandomXForPosition() {
-    return Math.floor(Math.random() * (sizes.width - 300)); 
+    return Math.floor(Math.random() * (sizes.width - 300));
   }
 }
 
